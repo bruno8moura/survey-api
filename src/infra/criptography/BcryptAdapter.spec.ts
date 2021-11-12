261622391286bfcc06eebce9ec68b80faef7c88e
Bruno Moura <bruno8moura@gmail.com>
2021-11-12 05:13:30 -0300

    test: ensure BcryptAdapter returns a hash on success

--- src/infra/criptography/BcryptAdapter.spec.ts
+++ src/infra/criptography/BcryptAdapter.spec.ts
@@ -9,4 +9,12 @@ describe('Bcrypt Adapter', () => {
     await sut.encrypt('any_value')
     expect(hashSpy).toHaveBeenCalledWith('any_value', salt)
   })
+
+  test('should return a hash on success', async () => {
+    const salt = 12
+    const sut = new BcryptAdapter(salt)
+    jest.spyOn(sut, 'encrypt').mockReturnValueOnce(Promise.resolve('hashed_value'))
+    const hashedValue = await sut.encrypt('any_value')
+    expect(hashedValue).toBe('hashed_value')
+  })
 })
