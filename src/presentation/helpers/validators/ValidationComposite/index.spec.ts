2f55ea6cbc00d42650aa4ae9248e7eee0b9a3078
Bruno Moura <bruno8moura@gmail.com>
2021-12-07 05:47:06 -0300

    test: ensure ValidationComposite returns the first error if more than one validation fails

--- src/presentation/helpers/validators/ValidationComposite/index.spec.ts
+++ src/presentation/helpers/validators/ValidationComposite/index.spec.ts
@@ -1,22 +1,48 @@
 import { ValidationComposite } from '.'
 import { Validation } from '../Validation'
 
+interface SutTypes {
+  sut: ValidationComposite
+  validationStubs: Validation[]
+}
+
+const makeValidationStub = (): Validation => {
+  class ValidationStub implements Validation {
+    validate (input: any): Error {
+      return undefined
+    }
+  }
+
+  return new ValidationStub()
+}
+
+const makeSut = (): SutTypes => {
+  const validationStubs = [makeValidationStub(), makeValidationStub()]
+
+  const sut = new ValidationComposite(validationStubs)
+
+  return {
+    sut,
+    validationStubs
+  }
+}
+
 describe('Validation Composite', () => {
   test('should return an error if any validation failed', () => {
-    class ValidationStub implements Validation {
-      validate (input: any): Error {
-        return new Error('Validation Error!')
-      }
-    }
-
-    const validations = [
-      new ValidationStub()
-    ]
-
+    const { sut, validationStubs } = makeSut()
+    jest.spyOn(validationStubs[0], 'validate').mockReturnValue(new Error('A validation error!'))
     const input = { any_field: 'any_value' }
-    const sut = new ValidationComposite(validations)
     const error = sut.validate(input)
 
     expect(error).toBeTruthy()
+  })
+  test('should return the first error if more than one validation fails', () => {
+    const { sut, validationStubs } = makeSut()
+    jest.spyOn(validationStubs[0], 'validate').mockReturnValue(new Error('A validation error1!'))
+    jest.spyOn(validationStubs[1], 'validate').mockReturnValue(new Error('A validation error2!'))
+    const input = { any_field: 'any_value' }
+    const error = sut.validate(input)
+
+    expect(error).toEqual(new Error('A validation error1!'))
   })
 })
