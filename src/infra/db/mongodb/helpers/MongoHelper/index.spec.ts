be2a7da1b0d24bda2bc8715537c05d8a4f757dd9
Bruno Moura <bruno8moura@gmail.com>
2021-12-08 05:40:57 -0300

    test: ensure MongoHelper.modelMap returns field 'id' instead of '_id'

--- src/infra/db/mongodb/helpers/MongoHelper/index.spec.ts
+++ src/infra/db/mongodb/helpers/MongoHelper/index.spec.ts
@@ -12,8 +12,20 @@ describe('Mongo Helper', () => {
   test('should reconnect if mongodb is down', async () => {
     let accountCollection = await sut.getCollection('accounts')
     expect(accountCollection).toBeTruthy()
+
     await sut.disconnect()
     accountCollection = await sut.getCollection('accounts')
     expect(accountCollection).toBeTruthy()
+  })
+
+  test('should return field "id" instead of "_id"', async () => {
+    const accountCollection = await sut.getCollection('accounts')
+    expect(accountCollection).toBeTruthy()
+    const newJson = {
+      a: '1'
+    }
+    await accountCollection.insertOne(newJson)
+    const modeledNewJson = sut.modelMap({ data: newJson })
+    expect(modeledNewJson).toEqual({ id: modeledNewJson.id, a: newJson.a })
   })
 })
